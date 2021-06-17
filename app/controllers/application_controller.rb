7d2cb8317cbed99dd7ef40a5691e084e24808888
Su Yi Fan <suyifan2121>
2021-06-17 14:19:10 +0800

    added error handling for access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # catch access denied exception thrown by cancancan gem and display error message gracefully
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
+  end
 end
