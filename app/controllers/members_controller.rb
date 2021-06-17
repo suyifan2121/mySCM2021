b8c15bd3fefc2853642bd26edd53a991592988cd
Su Yi Fan <suyifan2121>
2021-06-17 14:50:07 +0800

    added role selection during member creation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -18,7 +18,10 @@ class MembersController < ApplicationController
     @member = Member.new(member_params.except(:password, :password_confirmation))
     if @member.save
       # create a new user for this particular member
-      @user = User.new(user_params)
+      @user = User.new(user_params.except(:role)) #exclude role as we dont use "role" column for user table
+
+      # update the user role
+      set_user_role(params[:member][:role])
       @user.save
 
       redirect_to :root, notice: 'Member was successfully created.'
@@ -51,6 +54,22 @@ class MembersController < ApplicationController
     end
 
     def member_params
-      params.require(:member).permit(:name, :email, :phone, :password, :password_confirmation)
+      params.require(:member).permit(:name, :email, :phone, :password, :password_confirmation, :role)
+    end
+
+    # set user role based on roles selected while creating member
+    def set_user_role(role)
+      case role
+      when "superadmin"
+        @user.superadmin_role = true
+      when "sysadmin"
+        @user.sysadmin_role = true
+      when "inventoryadmin"
+        @user.inventoryadmin_role = true
+      when "purchasingagent"
+        @user.purchasingagent_role = true
+      when "salesagent"
+        @user.salesagent_role = true
+      end
     end
 end
