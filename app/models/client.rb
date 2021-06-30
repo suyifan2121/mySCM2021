ab97094dfea9d0a020c967636e334becb27d571b
Su Yi Fan <suyifan2121>
2021-06-30 17:34:31 +0800

    Added validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
   has_many :orders
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true
   validates :phone, presence: true
 
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,8 +3,8 @@ class Member < ApplicationRecord
   has_many :orders
   has_many :items, through: :orders
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :phone, presence: true
 
   def get_role
